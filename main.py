9dc7c2b71498c8f9ed94bcaea358c0572945461a
nick <kongarsia@gmail.com>
2017-08-10 15:33:40 -0700

    add mechanize source

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import re
 import mechanize
 
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+test = br.open('http://www.korrespondent.com')
+print test.read()
